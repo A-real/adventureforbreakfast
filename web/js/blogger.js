7380a266125f395e46af15d9893accd9a1f770ab
Eric Nolte <amishe@gmail.com>
2014-05-01 10:02:56 -0400

    update blogger id, css for links

--- web/js/blogger.js
+++ web/js/blogger.js
@@ -1,5 +1,5 @@
 var apiKey = "AIzaSyBqUaquZh_9bXxCYQr5T1eruhABr9fPG4w"
-var blogID = "3887994559066526603"
+var blogID = "3295796553627316717"
 var bloggerAPI = "https://www.googleapis.com/blogger/v2/blogs/" +
                 blogID +
                 "/posts?callback=?&key=" +
