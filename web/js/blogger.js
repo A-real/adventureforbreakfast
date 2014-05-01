3edff8e0ae7009492a80cd965d4e5d65bfc35d4d
Eric Nolte <amishe@gmail.com>
2014-05-01 09:44:28 -0400

    add js files and blogger.js

---
+++ web/js/blogger.js
@@ -0,0 +1,15 @@
+var apiKey = "AIzaSyBqUaquZh_9bXxCYQr5T1eruhABr9fPG4w"
+var blogID = "3887994559066526603"
+var bloggerAPI = "https://www.googleapis.com/blogger/v2/blogs/" +
+                blogID +
+                "/posts?callback=?&key=" +
+                apiKey;
+var response = $.getJSON(bloggerAPI).done(
+  function ( data ) {
+    for (var i=0;i < 4;i++) {
+    	$('#post' + i).append($('<a href="' +
+                  response.responseJSON.items[i].url + '">' +
+                  response.responseJSON.items[i].title +'</a>'));
+    };
+  }
+);
