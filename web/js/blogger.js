cc494e77438877eca6acdc5279d597e3e174b7a8
Eric Nolte <amishe@gmail.com>
2014-05-14 17:47:22 -0400

    update css for better blog fit, change blog wells for buttons

--- web/js/blogger.js
+++ web/js/blogger.js
@@ -2,14 +2,43 @@ var apiKey = "AIzaSyBqUaquZh_9bXxCYQr5T1eruhABr9fPG4w"
 var blogID = "3295796553627316717"
 var bloggerAPI = "https://www.googleapis.com/blogger/v2/blogs/" +
                 blogID +
-                "/posts?callback=?&key=" +
+                "/posts?alt=json&callback=?&key=" +
                 apiKey;
 var response = $.getJSON(bloggerAPI).done(
   function ( data ) {
     for (var i=0;i < 4;i++) {
-    	$('#post' + i).append($('<a href="' +
+    	$('#post' + i).append($('<a class="button button-block button-rounded button-large" href="' +
                   response.responseJSON.items[i].url + '">' +
                   response.responseJSON.items[i].title +'</a>'));
     };
   }
 );
+
+
+//Experimental Picasa Code //
+
+// $(function(){
+//     var $ul = $('ul');
+//     $.ajax({
+//         dataType: 'jsonp',
+//         url: 'https://picasaweb.google.com/data/feed/api/user/takazudo/albumid/5579032834644034737',
+//         data: {
+//             alt: 'json-in-script',
+//         },
+//         jsonpCallback: 'picasaCallback',
+//         success: function(data){
+//             var photos = [];
+//             $.each(data.feed.entry, function(){
+//                 photos.push({
+//                     src: this.content.src
+//                 });
+//             });
+//             $('#template').tmpl({
+//                 photos: photos
+//             }).appendTo('#output');
+//         },
+//         error: function(){
+//             alert('failed ;(');
+//         }
+//     });
+// });
